9bea09bbaeb0b51ff0242689a982db2dace6abdc
zcubbs <zackcubby@gmail.com>
2024-01-23 00:02:54 +0100

    :zap: add converter tests

---
+++ cmd/server/api/converter_test.go
@@ -0,0 +1,78 @@
+package api
+
+import (
+	"google.golang.org/protobuf/types/known/timestamppb"
+	"testing"
+	"time"
+
+	"github.com/stretchr/testify/assert"
+	db "github.com/zcubbs/grill/cmd/server/db/sqlc"
+	pb "github.com/zcubbs/grill/gen/proto/go/grill/v1"
+)
+
+func TestConvertUserToPb(t *testing.T) {
+	// Setup
+	dbUser := db.User{
+		Username:          "testUser",
+		FullName:          "Test User",
+		Email:             "test@example.com",
+		PasswordChangedAt: time.Now(),
+		CreatedAt:         time.Now(),
+		Role:              "ROLE_USER",
+	}
+
+	// Call the function
+	pbUser := convertUserToPb(dbUser)
+
+	// Assertions
+	assert.Equal(t, dbUser.Username, pbUser.GetUsername())
+	assert.Equal(t, dbUser.FullName, pbUser.GetFullName())
+	assert.Equal(t, dbUser.Email, pbUser.GetEmail())
+	assert.Equal(t, dbUser.PasswordChangedAt.UTC(), pbUser.GetPasswordChangedAt().AsTime())
+	assert.Equal(t, dbUser.CreatedAt.UTC(), pbUser.GetCreatedAt().AsTime())
+	assert.Equal(t, dbUser.Role, pbUser.GetRole().String())
+}
+
+func TestConvertPbToUser(t *testing.T) {
+	// Setup
+	pbUser := &pb.User{
+		Username:          "testUser",
+		FullName:          "Test User",
+		Email:             "test@example.com",
+		PasswordChangedAt: timestamppb.Now(),
+		CreatedAt:         timestamppb.Now(),
+		Role:              pb.Role_ROLE_USER,
+	}
+
+	// Call the function
+	dbUser := convertPbToUser(pbUser)
+
+	// Assertions
+	assert.Equal(t, pbUser.GetUsername(), dbUser.Username)
+	assert.Equal(t, pbUser.GetFullName(), dbUser.FullName)
+	assert.Equal(t, pbUser.GetEmail(), dbUser.Email)
+	assert.Equal(t, pbUser.GetPasswordChangedAt().AsTime(), dbUser.PasswordChangedAt.UTC())
+	assert.Equal(t, pbUser.GetCreatedAt().AsTime(), dbUser.CreatedAt.UTC())
+	assert.Equal(t, pbUser.GetRole().String(), dbUser.Role)
+}
+
+func TestConvertPbToAgent(t *testing.T) {
+	// Setup
+	pbAgent := &pb.Agent{
+		Name:   "testAgent",
+		Active: false,
+		Group:  "testGroup",
+		Token:  "testToken",
+		Scopes: "testScopes",
+	}
+
+	// Call the function
+	dbAgent := convertPbToAgent(pbAgent)
+
+	// Assertions
+	assert.Equal(t, pbAgent.GetName(), dbAgent.Name)
+	assert.Equal(t, pbAgent.GetActive(), dbAgent.Active)
+	assert.Equal(t, pbAgent.GetGroup(), dbAgent.Group)
+	assert.Equal(t, pbAgent.GetToken(), dbAgent.Token)
+	assert.Equal(t, pbAgent.GetScopes(), dbAgent.Scopes)
+}
